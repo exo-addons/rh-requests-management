f3ce1f363cc98c78f27d2818498354ef30ba3d5d
LamiaBouhajja <lbouhajja@exoplatform.com>
2017-03-09 13:15:35 +0100

    RH Admin Improvement:       Description :          -> Disable datePicker format          -> Add LoadingRequests on GetUser function

--- portlet/src/main/java/org/exoplatform/rhmanagement/portlet/rhAdministration/assets/js/rh-admin.js
+++ portlet/src/main/java/org/exoplatform/rhmanagement/portlet/rhAdministration/assets/js/rh-admin.js
@@ -88,7 +88,7 @@ require( ["SHARED/jquery", "rhAdminAddonControllers"], function ( $,  rhAdminCon
 	});
 
     $( "#startDate, #birthDay, #leaveDate, #contractStartDate, #contractEndDate" ).datepicker();
-    $( "#startDate, #birthDay, #leaveDate, #contractStartDate, #contractEndDate" ).datepicker( "option", "dateFormat", "yy-mm-dd" );
+//    $( "#startDate, #birthDay, #leaveDate, #contractStartDate, #contractEndDate" ).datepicker( "option", "dateFormat", "yy-mm-dd" );
     $( "#startDate, #birthDay, #leaveDate, #contractStartDate, #contractEndDate" ).datepicker( "option", "changeYear", true );
     $( "#startDate, #birthDay, #leaveDate, #contractStartDate, #contractEndDate" ).datepicker( "option", "yearRange", "1970:c+nn" );
 
